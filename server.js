a23e96d3468288d653ca43a11df6957b824a90dd
Tanay Raj <tanay.raj76@gmail.com>
2021-02-02 22:09:46 +0530

    second commit

--- server.js
+++ server.js
@@ -6,22 +6,29 @@ const app = express();
 let server = http.createServer(app);
 let io = socketIO(server);
 
+app.set("view engine","ejs");
 //path.join() method joins the specified path segments into one path
 const publicPath = path.join(__dirname,"/public")
 //To serve static files such as images, CSS files, and JavaScript files
 app.use(express.static(publicPath));
 
+app.set("view engine","ejs");
 
-io.on('connection',(socket)=>{
-    console.log("Connected successfully");
 
-    socket.on('toggle',(classname)=>{
-        io.emit('change',{
-            className:classname.nameofclass,
-            user:classname.user
+app.get('/',(req,res)=>{
+    res.render("index")
+    io.on('connection',(socket)=>{
+        console.log("Connected successfully");
+    
+        socket.on('toggle',(classname)=>{
+            io.emit('change',{
+                className:classname.nameofclass,
+                user:classname.user
+            })
         })
     })
 })
+
 
 
 
