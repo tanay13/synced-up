1f76c72541157c81548f0945df9216a24afa6ce0
Tanay Raj <tanay.raj76@gmail.com>
2021-02-23 22:20:19 +0530

    UI chages

--- public/js/youtube.js
+++ public/js/youtube.js
@@ -1,83 +1,86 @@
-// const form = document.getElementById('search-box')
-        // const myIframe = document.getElementById('iframe')
-        // form.addEventListener('submit',(e)=>{
-        //     e.preventDefault()
-        //     var url = form.search.value;
-        //     console.log(url)
-        //     var linkID = url.slice(17)
-        //     document.getElementById("iframe").src = "https://www.youtube.com/embed/"+linkID
-        //     const frameDoc = myIframe.contentWindow
-        //     console.log(frameDoc)
-        //     form.search.value = " ";
-        // })
+const search = document.getElementById('link')
+const subBtn = document.getElementById('btn')
 
-        // 2. This code loads the IFrame Player API code asynchronously.
-        var tag = document.createElement('script');
-        let socket = io();
-        tag.src = "https://www.youtube.com/iframe_api";
-        var firstScriptTag = document.getElementsByTagName('script')[0];
-        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-        var player;
-        function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-            height: '390',
-            width: '640',
-            videoId: 'M7lc1UVf-VE',
-            playerVars : {
-                controls : 0
-            },
-            events: {
-            // 'onReady': onPlayerReady,
-            // 'onStateChange': onPlayerStateChange
-            }
-        });
-        }
+// var linkID = 'M7lc1UVf-VE'
+var linkID
+subBtn.addEventListener('click',(e)=>{
+    e.preventDefault()
+    var url = search.value;
+    var linkID = url.slice(17)
+    console.log("clicked")
+    search.value = " ";
+})
+console.log(linkID)
 
-        // 4. The API will call this function when the video player is ready.
-        function onPlayerReady(event) {
-        event.target.playVideo();
-        }
-        function stopVideo() {
-            player.stopVideo();
-        }
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+let socket = io();
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-        var btn = document.getElementById('yt')
-        var state = document.getElementById('state')
-        done = false;
-        
-        var dur = 0
+var player;
 
-        socket.on('change',(event)=>{
-            if(event.e === 'play')
-            {
-                state.innerHTML = "PAUSE"
-                player.playVideo()
-            }
-            else{
-                state.innerHTML = "PLAY"
-                player.pauseVideo()
-            }
-            
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+    height: '390',
+    width: '640',
+    videoId: linkID ,
+    playerVars : {
+        controls : 0
+    },
+    events: {
+    // 'onReady': onPlayerReady,
+    // 'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+event.target.playVideo();
+}
+function stopVideo() {
+    player.stopVideo();
+}
+
+var btn = document.getElementById('yt')
+var state = document.getElementById('state')
+done = false;
+
+var dur = 0
+
+socket.on('change',(event)=>{
+    if(event.e === 'play')
+    {
+        state.innerHTML = "PAUSE"
+        player.playVideo()
+    }
+    else{
+        state.innerHTML = "PLAY"
+        player.pauseVideo()
+    }
+    
+})
+
+
+
+btn.addEventListener('click',()=>{
+    if(done){
+        player.pauseVideo()
+        state.innerHTML = "PLAY"
+        socket.emit('yevent',{
+            event : "pause"  
         })
-
-
-
-        btn.addEventListener('click',()=>{
-            if(done){
-                player.pauseVideo()
-                state.innerHTML = "PLAY"
-                socket.emit('yevent',{
-                    event : "pause"  
-                })
-                done = false
-            }
-            else{
-                player.playVideo()
-                state.innerHTML = "PAUSE"
-                socket.emit('yevent',{
-                    event : "play"  
-                })
-                done = true
-            }
+        done = false
+    }
+    else{
+        player.playVideo()
+        state.innerHTML = "PAUSE"
+        socket.emit('yevent',{
+            event : "play"  
         })
-        
+        done = true
+    }
+})
+
