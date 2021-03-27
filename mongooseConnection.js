f2003f0c0b26437f56d9e1224bef2906107ff242
Tanay Raj <tanay.raj76@gmail.com>
2021-03-27 21:52:01 +0530

    cloud db connected

--- mongooseConnection.js
+++ mongooseConnection.js
@@ -1,6 +1,11 @@
+if(process.env.NODE_ENV != "production")
+{
+  require('dotenv').config();
+}
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/synced-up', {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=>{
         console.log("DATABASE CONNECTED")
     })
